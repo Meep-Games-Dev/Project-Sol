8bf4510bf2d2179cb06fea02190acd1e81bc805c
Gus <guspearson78@gmail.com>
2025-10-01 08:48:17 -0700

    Update RootSpaceStationPeice.cs

--- RootSpaceStationPeice.cs
+++ RootSpaceStationPeice.cs
@@ -5,8 +5,10 @@ public class RootSpaceStationPeice : MonoBehaviour
 {
     public int X;
     public int Y;
-    public Script OtherSpaceStationPeices; //have OtherSpaceStationPeices be a link to all peices
-    //OtherSpaceStationPeice will be a function that does the bundling and returns the selected peice
+    public Script OtherSpaceStationPeices;//have OtherSpaceStationPeices.cs be a link to all peices
+//OtherSpaceStationPeice will be a function that does the bundling and returns the selected peice
+//when a peice is selected, it will be stored in OtherSpaceStationPeice
+//I need a int for X, Y, and Rotation for OtherSpaceStationPeice
     OtherSpaceStationPeices OtherSpaceStationPeice;
     public Sprite me;
     
@@ -15,13 +17,15 @@ public class RootSpaceStationPeice : MonoBehaviour
         X = 0;
         Y = 0;
     }
-    public void OnCollision(){
-        if (OtherSpaceStationPeice.X == X && OtherSpaceStationPeice.Y == Y)
+    public void OnCollision(Collision collision){
+        GameObject otherObj = collision.gameObject;
+        if ((OtherSpaceStationPeice.X == X && OtherSpaceStationPeice.Y == Y) || otherObj )
         {
-            me = me += OtherSpaceStationPeice; // selected object becomes OtherSpaceStationPeice
+            
+            me = me += OtherSpaceStationPeice;
             //wait no this doesnt work, AHHHHHHHHHH. 
             //make it so if their touching, not on top of each other!!!!!
-            
+
         }
     }
 
