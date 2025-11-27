838cb6423da6cdbdd030d2166c4419da8b2d8059
Gus <guspearson78@gmail.com>
2025-11-26 18:51:47 -0800

    added stuff
    who even writes this stuff?

---
+++ Assets/Gus/SpaceStation.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class SpaceStation : Monobehavior // add to unity project
+{
+    public OtherSpaceStationPeices referance;
+    public RootSpaceStationPeice referance;
+    public Structual_piece referance;
+    // referance all pieces' script here.
+}
