65e2bdadbff3a2ae26efcefc8a247d71c2a244de
Gus <guspearson78@gmail.com>
2025-12-22 07:54:22 -0800

    Update Structual_piece.cs

--- Assets/Gus/Structual_piece.cs
+++ Assets/Gus/Structual_piece.cs
@@ -1,19 +1,17 @@
 using UnityEngine;
 using System.Collections.Generic;
-//using OGPC-2025-2026.Assets.Gus;
-//using OGPC BUGGY CODE TEST.scripts;
 
+public class Structual_piece
+{
+    int X = 0;
+    int Y = 0;
+    int R = 0;
+}
 public class Structual_piece : MonoBehaviour
 {
-    void Start(){
-        int X = 0;
-        int Y = 0;
-        int R = 0;
-    }
-    void Update(){
-        if (Input.GetMouseButtonDown(0)){ //probably not needed
-            Vector3 MousePos = Input.mousePosition;
-            transform.Translate(Vector3.mousePosition);
 
+    void Activation()//on build.cs or RootSpaceStation.cs call Structual_piece.Activation(); to start the function
+    {
+        //control for the piece
     }
 }
