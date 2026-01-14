0657fb78f1bb7307532c7a7d443c3bbae8272091
Gus <guspearson78@gmail.com>
2026-01-13 18:26:05 -0800

    Refactor Structual_piece class and add namespace

--- Assets/Gus/Structual_piece.cs
+++ Assets/Gus/Structual_piece.cs
@@ -1,17 +1,26 @@
 using UnityEngine;
 using System.Collections.Generic;
+using SpaceStation;
 
-public class Structual_piece
+namespace Structual_Piece
 {
-    int X = 0;
-    int Y = 0;
-    int R = 0;
-}
-public class Structual_piece : MonoBehaviour
-{
-
-    void Activation()//on build.cs or RootSpaceStation.cs call Structual_piece.Activation(); to start the function
+    public class Structual_piece : MonoBehaviour
     {
-        //control for the piece
+        int X = 0;
+        int Y = 0;
+        int R = 0;
+        void update()
+        {
+            /* if (X > canvas_max_X || X < canvas_min_X || Y > canvas_max_Y || Y < canvas_min_Y)
+            {
+                X = 0;
+                Y = 0;
+            }
+            */
+        }
+        void Activation()//on build.cs or RootSpaceStation.cs call Structual_piece.Activation(); to start the function
+        {
+            //control for the piece
+        }
     }
 }
