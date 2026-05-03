0066f80f53198c7a33656cce23e0843116e64283
Gus <guspearson78@gmail.com>
2026-05-02 18:51:42 -0700

    Changed stuff

--- Assets/Gus/SpaceStation.cs
+++ Assets/Gus/SpaceStation.cs
@@ -19,27 +19,26 @@ namespace StationO
         public int peoplelimit = 100;
         public RayController raycontroller;
         public static List<GameObject> ATpiece = new List<GameObject>();
-        public static Dictionary<ResourceType, int> resources = new Dictionary<ResourceType, int>()
+        public static Dictionary<Resources, int> resources = new Dictionary<Resources, int>()
         {   
-            {ResourceType.O2, 0}, //oxygen 
-            {ResourceType.Ore, 0}, //ore -> metals 
-            {ResourceType.H2O, 0}, //water
-            {ResourceType.Carbon, 0}, //carbon
-            {ResourceType.Food, 0}, //food
-            {ResourceType.Metals, 0}, // Refined Metal
-            {ResourceType.People, 50},
-            {ResourceType.Credits, 0}, // Credits, affected in ResorceMiner and Hanger/trade modules
-            {ResourceType.PowerCells, 5} // power, 1 solar panel array gives + 10 power, each module gives -1 power and you can transmit power to other stations
+            {Resources.Ice, 0},
+            {Resources.Ore, 0}, //ore -> metals 
+            {Resources.H2O, 0}, //water
+            {Resources.Carbon, 0}, //carbon
+            {Resources.Food, 0}, //food
+            {Resources.RefinedMetal, 0}, // Refined Metal
+            {Resources.Population, 50},
+            {Resources.Credits, 0}, // Credits, affected in ResorceMiner and Hanger/trade modules
+            {Resources.EnergyCells, 5} // pow er, 1 solar panel array gives + 10 power, each module gives -1 power and you can transmit power to other stations
         };
         //public static List<GameObject> ATpiece; // make 
         void createstation(Station station)
         { 
-            if(resources[ResourceType.Credits] >= 1000 && resources[ResourceType.O2] >= 500 && resources[ResourceType.H2O] >= 500 && resources[ResourceType.Carbon] >= 500) // check if the player has enough resources to create the station
+            if(resources[Resources.Credits] >= 1000 && resources[Resources.H2O] >= 500 && resources[Resources.Carbon] >= 500) // check if the player has enough resources to create the station
             {
                 credits -= 1000; // subtract the credits
-                resources[ResourceType.O2] -= 500; // subtract the O2
-                resources[ResourceType.H2O] -= 500; // subtract the H2O
-                resources[ResourceType.Carbon] -= 500; // subtract the Carbon
+                resources[Resources.H2O] -= 500; // subtract the H2O
+                resources[Resources.Carbon] -= 500; // subtract the Carbon
                 people -= 50; // subtract the people
                 
             }
@@ -47,18 +46,20 @@ namespace StationO
 
         void Update()
         {
-            resources[ResourceType.Credits] = credits;
-            resources[ResourceType.People] = people;
-            if (resources[ResourceType.Food] >= 20) // refines food into people
+            resources[Resources.Credits] = credits;
+            resources[Resources.Population] = people;
+            if (resources[Resources.Food] >= 20) // refines food into people
             {
                 if(people < peoplelimit)
                 {
-                    resources[ResourceType.Food] -= 20;
+                    resources[Resources.Food] -= 20;
                     people += 1;
                 }
             }
             foreach(var s in raycontroller.stations)
             {
+                credits += s.resources[Resources.Credits];
+                s.resources[Resources.Credits] = 0;
                 foreach (var u in s.pieces)
                 {
                     IActivable e = u.GetComponent<IActivable>();
